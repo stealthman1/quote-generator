fa3974ddfb4d60dcc56ae23adc140b2302a2c9e7
stealthman1 <timur.marufiy@gmail.com>
2022-07-29 13:07:04 +0300

    adding buttons functionality

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote)
+
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    }   else {
+            authorText.textContent = quote.author;
+        }
+    // Check Quote lenght to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-auote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -18,6 +38,16 @@ async function getQuotes() {
         
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
